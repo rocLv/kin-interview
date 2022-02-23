7167d2249249dabb937da7042e855b328e70c9f0
rocLv <wangqsh999@gmail.com>
2022-02-23 11:19:46 +0800

    feature: user register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,28 @@
 class RegistrationsController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def create
+    @user = User.create(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  def show
   end
 
   def new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,13 +1,18 @@
 require "test_helper"
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    post registrations_url
-    assert_response :success
-  end
-
   test "should get new" do
     get new_registration_url
     assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      post registrations_url, params: {
+        user: {
+          username: "new_user"
+        }
+      }
+    end
   end
 end
